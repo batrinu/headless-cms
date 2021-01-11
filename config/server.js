70d9e22b2c72b45809ee5c8465df68f54adede73
batrinu <batrinu@outlook.com>
2021-01-11 22:56:53 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a56f447f55871eb1036b34573f4f55ba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '55942786-4c15-4602-a679-10df2b383b31'
+};
